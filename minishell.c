c24894cf0d80bd53828d08768be9adbc2c70866f
TastyBao <erard.bastien@gmail.com>
2023-03-22 17:43:48 +0100

    Update - Handle the signals

--- minishell.c
+++ minishell.c
@@ -3,33 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fgrasset <fgrasset@student.42.fr>          +#+  +:+       +#+        */
+/*   By: berard <berard@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 13:37:06 by berard            #+#    #+#             */
-/*   Updated: 2023/03/20 17:16:16 by fgrasset         ###   ########.fr       */
+/*   Updated: 2023/03/22 17:42:31 by berard           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int main(int ac, char **av, char **env)
+int	main(int ac, char **av, char **env)
 {
-	char	*input;
+	char			*input;
+	struct termios	termios;
 	// t_token	*head;
 
 	(void)ac;
 	(void)av;
 	(void)env;
+	if ((tcgetattr(STDIN_FILENO, &termios)) == -1)
+		exit(EXIT_FAILURE);
+	termios.c_lflag &= ~(ECHOCTL);
+	if ((tcsetattr(STDIN_FILENO, TCSANOW, &termios)) == -1)
+		exit(EXIT_FAILURE);
 	input = "lol";
-
-	signals_init(); // TODO
 	while (input != NULL)
 	{
+		signals_init(); // TODO
 		input = readline("Minishell > ");
 		// head = tokenize(input);
 		// print_list(head);
 		add_history(input);
 	}
 	free(input);
-	return(0);
+	return (0);
 }
